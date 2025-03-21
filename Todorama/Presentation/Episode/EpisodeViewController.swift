d66273ff52db9c74575437fb84f58cbe367d7e1c
claire <clairebluepark@gmail.com>
2025-03-21 17:15:26 +0900

    feat: 시리즈 화면 뷰 배치

---
+++ Todorama/Presentation/Episode/EpisodeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EpisodeViewController.swift
+//  Todorama
+//
+//  Created by Claire on 3/21/25.
+//
+
+import UIKit
+
+final class EpisodeViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+}

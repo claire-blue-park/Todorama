d7b8cc7a22417e22ba3d12115c37a18666e2b885
woosuk <kevincool6@naver.com>
2025-03-23 13:42:56 +0900

    add: CommentTableViewCell, CommentViewModel 추가

---
+++ Todorama/Presentation/Comment/CommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentTableViewCell.swift
+//  Todorama
+//
+//  Created by 권우석 on 3/23/25.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Todorama/Presentation/Comment/CommentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommentViewModel.swift
+//  Todorama
+//
+//  Created by 권우석 on 3/23/25.
+//
+
+import Foundation
